83da6de7fd7b9ac34aadb6b8570b16118fb02857
Nathan Fixler <nathan@fixler.org>
2011-09-19 16:35:18 -0400

    Subclass rails engine to expose assets

--- lib/codemirror/rails.rb
+++ lib/codemirror/rails.rb
@@ -2,6 +2,8 @@ module Codemirror
   module Rails
     if ::Rails.version < "3.1"
       require 'codemirror/rails/railtie'
+    else
+      require 'codemirror/rails/engine'
     end
     require 'codemirror/rails/version'
   end
---
+++ lib/codemirror/rails/engine.rb
@@ -0,0 +1,6 @@
+module Codemirror
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
