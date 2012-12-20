e89540fab7d0e699b45125eb412b4749203b8f64
Viliam Pucik <viliam.pucik@gmail.com>
2012-12-20 14:55:48 +0100

    Update test search text

--- test/integration/codemirror_rails_integration_test.rb
+++ test/integration/codemirror_rails_integration_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 describe 'codemiror-rails integration' do
   it 'provides codemirror.js on the asset pipeline' do
     visit '/assets/codemirror.js'
-    page.text.must_include 'var CodeMirror'
+    page.text.must_include 'window.CodeMirror'
   end
 
   it 'provides codemirror css on the asset pipeline' do
