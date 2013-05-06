5f71e08fed10027c05a75a044bc168410bfc6af8
Jonathan Soeder <jonathan.soeder@gmail.com>
2013-05-06 00:09:33 -0500

    Adding small hack to allow for use of this gem outside of rails

--- lib/codemirror-rails.rb
+++ lib/codemirror-rails.rb
@@ -1,1 +1,23 @@
+# When using outside of Rails.
+# Allows for using Rails asset pipline gems in other
+# projects setup to use sprockets, e.g. middlemanapp.com
+unless defined? ::Rails
+  module Rails
+    def self.version
+      "3.2.13"
+    end
+
+    def self.env
+      Class.new do
+        def test?
+          false
+        end
+      end.new
+    end
+
+    class Engine
+    end
+  end
+end
+
 require 'codemirror/rails'
