5f492716fe5b30ea9b589b20b87524db82b49dcd
Nathan Fixler <nathan@fixler.org>
2014-01-26 11:15:50 -0800

    Upgrade vendored assets to CodeMirror 3.17

--- vendor/assets/javascripts/codemirror/addons/tern/tern.js
+++ vendor/assets/javascripts/codemirror/addons/tern/tern.js
@@ -24,6 +24,9 @@
 //   no tip should be shown. By default the docstring is shown.
 // * typeTip: Like completionTip, but for the tooltips shown for type
 //   queries.
+// * responseFilter: A function(doc, query, request, error, data) that
+//   will be applied to the Tern responses before treating them
+//
 //
 // It is possible to run the Tern server in a web worker by specifying
 // these additional options:
@@ -102,8 +105,16 @@
 
     rename: function(cm) { rename(this, cm); },
 
-    request: function(cm, query, c) {
-      this.server.request(buildRequest(this, findDoc(this, cm.getDoc()), query), c);
+    request: function (cm, query, c) {
+      var self = this;
+      var doc = findDoc(this, cm.getDoc());
+      var request = buildRequest(this, doc, query);
+
+      this.server.request(request, function (error, data) {
+        if (!error && self.options.responseFilter)
+          data = self.options.responseFilter(doc, query, request, error, data);
+        c(error, data);
+      });
     }
   };
 
@@ -233,12 +244,24 @@
     closeArgHints(ts);
 
     if (cm.somethingSelected()) return;
-    var lex = cm.getTokenAt(cm.getCursor()).state.lexical;
+    var state = cm.getTokenAt(cm.getCursor()).state;
+    var inner = CodeMirror.innerMode(cm.getMode(), state);
+    if (inner.mode.name != "javascript") return;
+    var lex = inner.state.lexical;
     if (lex.info != "call") return;
 
-    var ch = lex.column, pos = lex.pos || 0;
-    for (var line = cm.getCursor().line, e = Math.max(0, line - 9), found = false; line >= e; --line)
-      if (cm.getLine(line).charAt(ch) == "(") {found = true; break;}
+    var ch, pos = lex.pos || 0, tabSize = cm.getOption("tabSize");
+    for (var line = cm.getCursor().line, e = Math.max(0, line - 9), found = false; line >= e; --line) {
+      var str = cm.getLine(line), extra = 0;
+      for (var pos = 0;;) {
+        var tab = str.indexOf("\t", pos);
+        if (tab == -1) break;
+        extra += tabSize - (tab + extra) % tabSize - 1;
+        pos = tab + 1;
+      }
+      ch = lex.column - extra;
+      if (str.charAt(ch) == "(") {found = true; break;}
+    }
     if (!found) return;
 
     var start = Pos(line, ch);
