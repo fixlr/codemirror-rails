098848e5ab067d352748b9e5013693bd814fb461
Mark Dickson <mark@sitesteaders.com>
2014-03-21 17:38:59 -0400

    liquid mode working as multiplex

--- vendor/assets/javascripts/codemirror/modes/liquid.js
+++ vendor/assets/javascripts/codemirror/modes/liquid.js
@@ -1,40 +1,18 @@
-/*
-  This overlay provides a 'liquid' mode to the excellent CodeMirror editor (http://codemirror.net/).
-  Add something like this to your CSS:
+CodeMirror.defineMode("liquid", function(config) {
 
-    .cm-liquid-tag {
-      color: #32273f;
-      background: #ead9ff;
+  return CodeMirror.multiplexingMode(
+    CodeMirror.getMode(config, "text/html"),
+    {
+      open: "{{", close: "}}",
+      mode: CodeMirror.getMode(config, "text/x-liquid-variable"),
+      delimStyle: "liquid variable variable2",
+      innerStyle: "liquid variable variable2"
+    },
+    {
+      open: "{%", close: "%}",
+      mode: CodeMirror.getMode(config, "text/x-liquid-tag"),
+      delimStyle: "liquid special variable2 keyword",
+      innerStyle: "liquid special variable2 keyword"
     }
-  
-    .cm-liquid-variable {
-      color: #29739b;
-      background: #c2e0f0;
-    }
-
-*/
-
-CodeMirror.defineMode("liquid", function(config, parserConfig) {
-  var liquidOverlay = {
-    token: function(stream, state) {
-
-      // Variables.
-      if (stream.match("{{")) {
-        while ((ch = stream.next()) != null)
-          if (ch == "}" && stream.next() == "}") break;
-        return "liquid-variable";
-      }
-      
-      // Tags.
-      if(stream.match("{%")) {
-        while ((ch = stream.next()) != null)
-          if (ch == "%" && stream.next() == "}") break;
-        return "liquid-tag";
-      }
-      
-      while (stream.next() != null && !stream.match("{{", false) && !stream.match("{%", false)) {}
-      return null;
-    }
-  };
-  return CodeMirror.overlayParser(CodeMirror.getMode(config, parserConfig.backdrop || "text/html"), liquidOverlay);
+  );
 });
