9c57828cdbdbecdddca110b7444d67388f3a0f1a
Nathan Fixler <nathan@fixler.org>
2014-07-31 17:29:10 -0700

    Add tests for Rails 4.1
    Resolves issue #29

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,8 @@ when '3.2'
   require File.expand_path('../dummy-3.2.x/config/environment', __FILE__)
 when '4.0'
   require File.expand_path('../dummy-4.0.x/config/environment', __FILE__)
+when '4.1'
+  require File.expand_path('../dummy-4.1.x/config/environment', __FILE__)
 end
 
 require 'rails/test_help'
