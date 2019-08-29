f77a8a8b8b73088046a6ec5c0a33d71b1a121278
Nathan Fixler <nathan@fixler.org>
2019-08-29 10:20:41 -0500

    Remove support for rails < 3.1
    Since the gemspec no longer allows rails < 4.2, the extra code to
    support rails 3.0 can be deleted.

--- lib/codemirror/rails.rb
+++ lib/codemirror/rails.rb
@@ -1,10 +1,6 @@
 module Codemirror
   module Rails
-    if ::Rails.version < "3.1"
-      require 'codemirror/rails/railtie'
-    else
-      require 'codemirror/rails/engine'
-    end
+    require 'codemirror/rails/engine'
     require 'codemirror/rails/version'
   end
 end
