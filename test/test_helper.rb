f6f53ef07470a842274607d2495510995c1f655f
Nathan Fixler <nathan@fixler.org>
2019-08-29 10:20:55 -0500

    load dummy for any version
    This commit trades in the hard-coded Rails versions for a more dynamic
    approach to loading the correct dummy application, and improves the
    error message that is displayed when attempting to run the test suite on
    a version of Rails that is not yet supported.
    
    To generate a new dummy application, start with `rails plugin --help`
    and review the list of available flags. Skip almost everyhing except
    sprockets.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,15 +1,21 @@
 ENV['RAILS_ENV'] = 'test'
 
 require 'rails/version'
-case "#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}"
-when '3.2'
-  require File.expand_path('../dummy-3.2.x/config/environment', __FILE__)
-when '4.0'
-  require File.expand_path('../dummy-4.0.x/config/environment', __FILE__)
-when '4.1'
-  require File.expand_path('../dummy-4.1.x/config/environment', __FILE__)
-when '5.0'
-  require File.expand_path('../dummy-5.0.x/config/environment', __FILE__)
+dummy_path = File.expand_path(
+  "dummy-#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}.x",
+  __dir__
+)
+
+if File.exist?(dummy_path)
+  require File.join(dummy_path, 'config', 'environment')
+else
+  puts <<-EOS
+
+  You must generate a dummy application for this version of Rails before you
+  can run the codemirror-rails test suite.
+
+  EOS
+  exit 1
 end
 
 require 'rails/test_help'
